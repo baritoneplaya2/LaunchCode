e2f64ba7e6f4d1a7dbfc7f3c28ce56ce170c91fd
Maria Vanessa Moreno <47546293+baritoneplaya2@users.noreply.github.com>
2019-09-30 21:28:16 -0400

    Create footballscores.py
    "The European Union has decided that they want to start an American football league, and they want to use your killer program to calculate scores, but they like things that are multiples of 10 (e.g. the Metric System), and have decided that touchdowns will be worth 10 points (including the extra point they might score) and field goals are worth 5 points. Modify the program below to work on both continents, and beyond. It should ask the user how many points a touchdown is worth and how many points a field goal is worth. Then it should ask in turn for both the number of touchdowns and the number of field goals scored, and then print the team’s total score."

---
+++ Python/week1/other/footballscores.py
@@ -0,0 +1,6 @@
+num_touchdowns = input("How many touchdowns were scored? ")
+num_field_goals = input("How many field goals were scored? ")
+
+total_score = 7 * int(num_touchdowns) + 3 * int(num_field_goals)
+
+print("The team has", total_score, "points")
